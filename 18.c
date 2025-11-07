f4b55963081b1354b85945863ac06420b52680d4
Dipanshu kumar <dipanshu.25scs1003002582@iilm.edu>
2025-11-07 23:12:49 +0530

    Add Armstrong number finder in 18.c
    Implement program to find Armstrong numbers between 1 and 1000.

---
+++ 18.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int number,temp,sum,remainder;
+    printf("The Armstrong number between 1-100 \n");
+    for(number=1;number<=1000;number++)
+    {
+        sum=0;
+        temp=number;
+        while(temp!=0)
+        {
+            remainder=temp%10;
+            sum=sum+(remainder*remainder*remainder);
+            temp=temp/10;
+        }
+        if(sum==number)
+            printf("%d \n",number);
+
+    }
+    return 0;
+}
