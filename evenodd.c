459602ca114449e2edbf5fa04271eff3d3518f93
Dipanshu kumar <dipanshu.25scs1003002582@iilm.edu>
2025-11-07 22:37:13 +0530

    Implement even or odd number checker

---
+++ evenodd.c
@@ -0,0 +1,14 @@
+// Write a program that finds whether a given number is even or odd.
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    if (number % 2 == 0) { //condition checking
+        printf("%d is an even number.\n", number);
+    } else {
+        printf("%d is an odd number.\n", number);
+    }
+    return 0;
+}
